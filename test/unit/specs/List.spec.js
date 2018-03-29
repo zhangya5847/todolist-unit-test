02898cdb58335b95f830bedd146dcf63c40a64f3
zhangya <zhangya5847@163.com>
2018-03-29 18:09:37 +0800

    使用官方推荐的测试库

--- test/unit/specs/List.spec.js
+++ test/unit/specs/List.spec.js
@@ -1,5 +1,6 @@
 import List from '@/components/List.vue'
 import Vue from 'vue'
+import { mount } from '@vue/test-utils'
 
 describe('List.vue', () => {
   it('display the items from the list', () => {
@@ -12,18 +13,27 @@ describe('List.vue', () => {
     expect(ListCompontent.$el.textContent).to.contain('sleep')
   })
   it('add a new item to list by click', () => {
-    // 1／build component
-    const Constructor = Vue.extend(List)
-    const ListCompontent = new Constructor().$mount()
-    // 2/ set value of new item
-    ListCompontent.newItem = 'play'
-    // 3/ click button
-    const button = ListCompontent.$el.querySelector('button')
-    const clickEvent = new window.Event('click')
-    button.dispatchEvent(clickEvent)
-    ListCompontent._watcher.run()
-    // 4/ check new item not only in the html but also in the list array
-    expect(ListCompontent.$el.textContent).to.contain('play')
-    expect(ListCompontent.list).to.contain('play')
+    // // 1／build component
+    // const Constructor = Vue.extend(List)
+    // const ListCompontent = new Constructor().$mount()
+    // // 2/ set value of new item
+    // ListCompontent.newItem = 'play'
+    // // 3/ click button
+    // const button = ListCompontent.$el.querySelector('button')
+    // const clickEvent = new window.Event('click')
+    // button.dispatchEvent(clickEvent)
+    // ListCompontent._watcher.run()
+    // // 4/ check new item not only in the html but also in the list array
+    // expect(ListCompontent.$el.textContent).to.contain('play')
+    // expect(ListCompontent.list).to.contain('play')
+
+    // 使用vue测试工具库
+    const wrapper =  mount(List)
+    // console.log(wrapper)
+    wrapper.vm.newItem = 'play'
+    wrapper.find('button').trigger('click')
+    expect(wrapper.text()).to.contain('play')
+    expect(wrapper.vm.list).to.contain('play')
+
   })
 })
