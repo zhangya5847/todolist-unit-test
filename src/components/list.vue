7eeb2d2a5e4f596d06005e114a63cd0ed0e4b178
zhangya <zhangya5847@163.com>
2018-03-29 15:55:38 +0800

    模拟用户输入

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>My Todo List</h1>
+    <input type="text" v-model="newItem">
+    <button @click="addItemToList">ADD</button>
     <ul>
       <li v-for="(item, i) in list" :key="i">{{ item }}</li>
     </ul>
@@ -12,7 +14,14 @@ export default {
   data() {
     return {
       list: ['eat food', 'work', 'sleep'],
+      newItem: '',
     }
+  },
+  methods: {
+    addItemToList() {
+      this.list.unshift(this.newItem)
+      this.newItem = ''
+    },
   },
 }
 </script>
--- test/unit/specs/List.spec.js
+++ test/unit/specs/List.spec.js
@@ -11,4 +11,19 @@ describe('List.vue', () => {
     // 这个断言检查HTML列表中的文本是否与组件data里的数据吻合
     expect(ListCompontent.$el.textContent).to.contain('sleep')
   })
+  it('add a new item to list by click', () => {
+    // 1／build component
+    const Constructor = Vue.extend(List)
+    const ListCompontent = new Constructor().$mount()
+    // 2/ set value of new item
+    ListCompontent.newItem = 'play'
+    // 3/ click button
+    const button = ListCompontent.$el.querySelector('button')
+    const clickEvent = new window.Event('click')
+    button.dispatchEvent(clickEvent)
+    ListCompontent._watcher.run()
+    // 4/ check new item not only in the html but also in the list array
+    expect(ListCompontent.$el.textContent).to.contain('play')
+    expect(ListCompontent.list).to.contain('play')
+  })
 })
